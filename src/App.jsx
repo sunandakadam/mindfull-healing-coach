0765ec9ac4bde3aaca2461bf53bf342e46deee49
sunandakadam <sunandak2007@gmail.com>
2026-04-29 16:52:57 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+   <h1>Higher Self Academy</h1>
+   <h3>Heal* Transform* Thrive</h3>
+   </>
+  )
+}
+
+export default App
